96adb210f392f4083daeee7a141840f9d462a72b
xxxprincemaker <princefelipe07@gmail.com>
2024-09-10 17:34:32 -0300

    initiate the project.

---
+++ src/main/java/com/example/recime/RecimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.recime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recime/RecimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
